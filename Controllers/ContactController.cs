29182a8b976957e172cc09e397cdbb849c4df891
Nkanyiso <sparxbantwan51@gmail.com>
2025-01-15 13:48:50 +0200

    Initial commit of .NET web app

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MPLsite.Models;
+
+namespace MPLsite.Controllers;
+
+public class ContactController : Controller
+{
+    private readonly ILogger<ContactController> _logger;
+
+    public ContactController (ILogger<ContactController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    
+   
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
